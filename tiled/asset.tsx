d28c0ef9ed3c58e0496a65f2222f4187186cce28
Garfunkle <T.blin@outlook.fr>
2022-05-03 23:31:17 +0200

    dossier map clean

---
+++ tiled/asset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="[Base]BaseChip_pipo" tilewidth="16" tileheight="16" tilecount="1992" columns="8">
+ <image source="../ressource tile/new asset/[Base]BaseChip_pipo.png" trans="003039" width="128" height="3984"/>
+</tileset>
